8a8b58a10658851956c19cda37aa1a7c2753b9f3
DESKTOP-94G8P2L\Chakir Belhaj <c.belhaj_zakelijk@hotmail.com>
2018-02-06 19:03:56 +0100

    saving changes

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -10,7 +10,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-         Console.Write();
+            var run = new Menu();
+            run.Run();
         }
     }
 }
