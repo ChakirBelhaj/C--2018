594d16bc82620b993f05561400d21323ec8909fe
DESKTOP-94G8P2L\Chakir Belhaj <c.belhaj_zakelijk@hotmail.com>
2018-02-07 00:03:14 +0100

    added class

---
+++ ConsoleApp1/ConsoleApp1/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Class1
+    {
+
+        public void Run()
+        {
+            bool loop = true;
+            while (loop) // Loop indefinitely
+            {
+                Console.WriteLine(" Please enter your choice! \n 1. Save your name \n 2. Count numbers \n 3. What is my name? \n 4. Exit program \n 5. PLEASE NO!"); // Menu
+            }
+        }
+    }
+}
