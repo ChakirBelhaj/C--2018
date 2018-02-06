a0062f743ebd86d90cecd043654240495f776429
DESKTOP-94G8P2L\Chakir Belhaj <c.belhaj_zakelijk@hotmail.com>
2018-02-06 17:17:28 +0100

    Pushing assignment 1

---
+++ ConsoleApp1/ConsoleApp1/les1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class les1
+    {
+        public les1()
+        {
+            Console.WriteLine("test");
+        }
+        public void Run()
+        {
+
+            Console.WriteLine("push enter");
+            while (ConsoleKey.Enter != Console.ReadKey().Key)
+            {
+
+                Console.WriteLine("komop dit is geen enter");
+
+            }
+        }
+    }
+
+
+}
