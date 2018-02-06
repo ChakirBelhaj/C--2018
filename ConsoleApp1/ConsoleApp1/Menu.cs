f05856afa05e4365dcf106605cb3b9f5d4bf1bcf
ChakirBelhaj <c.belhaj_zakelijk@hotmail.com>
2018-02-06 23:04:31 +0100

    pushing commented and completed assignment

--- ConsoleApp1/ConsoleApp1/Menu.cs
+++ ConsoleApp1/ConsoleApp1/Menu.cs
@@ -8,19 +8,80 @@ namespace ConsoleApp1
 {
     class Menu
     {
+        // declare public variable
+        public string name;
+
         public void Run()
         {
-            while (true) // Loop indefinitely
+            bool loop = true;
+            while (loop) // Loop indefinitely
             {
-                Console.WriteLine("Enter input:"); // Prompt
-                string line = Console.ReadLine(); // Get string from user
-                if (line == "exit") // Check string
+                Console.WriteLine(" Please enter your choice! \n 1. Save your name \n 2. Count numbers \n 3. What is my name? \n 4. Exit program"); // Menu
+                string option = Console.ReadLine(); // Get string from user
+                
+                switch (option) // switch statement
                 {
-                    break;
+                    case "1":
+                        saveName();
+                        break;
+                    case "2":
+                        countNumbers();
+                        break; 
+                    case "3":
+                        askName(name);
+                        break;
+                    case "4":
+                        loop = exit(loop);
+                        break;
+                   default:
+                       Console.WriteLine("please select an option");
+                       break;
                 }
-                else if (line != "")
+            }
+        }
+
+        public bool exit(bool loop)
+        {
+            loop = false;
+            return loop;
+        }
+        
+        //writ to consol and save name variable
+        public void saveName()
+        {
+            Console.WriteLine("Enter your name");   // Report output
+            name = Console.ReadLine();  //reads user input
+            Console.WriteLine("your new name = " + name + " \n");   // Report output
+        }
+
+        //if name is not empty, Report output
+        public void askName(string name)
+        {
+            if (name == "")
+            {
+                Console.WriteLine("There is no name saved!");// Report output
+            }
+            else
+            {
+                Console.WriteLine("your current name = " + name + " \n");// Report output
+            }
+        }
+
+        //  counts 2 user inputs
+        public void countNumbers()
+        {
+            string input = Console.ReadLine();
+            if (int.TryParse(input, out int inputInt))  //  checks if input is int
+            {
+                string secondInput = Console.ReadLine();
+                if (int.TryParse(secondInput, out int secondInputInt))  //  checks if input is int
                 {
-                    Console.Write("Your name is " + line + "\n"); // Report output
+                    int result;
+                    result = inputInt + secondInputInt;  //calculates 2 variables
+                    Console.Write( inputInt + " + " + secondInputInt + " = " + result + "\n"); // Report output
+                }else
+                {
+                    Console.Write("this is an int " + input + "\n"); // Report output
                 }
             }
         }
